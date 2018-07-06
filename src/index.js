a8cb001bbbbbcbf04672bc88a353b4a1550fbbc7
Quan Truong <qmtruong92@gmail.com>
2018-07-05 21:48:27 -0700

    Wrote function to generate a 3x3 layout

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-import 'typeface-roboto';
 import '../node_modules/react-grid-layout/css/styles.css';
 import '../node_modules/react-resizable/css/styles.css';
+import 'typeface-roboto';
 
 ReactDOM.render(<App />, document.getElementById('root'));
